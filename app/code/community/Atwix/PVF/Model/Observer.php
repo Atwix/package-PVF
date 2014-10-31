00346ca8e28981a78df71a90c8b458f0594f192a
yaro-atwix <enarc@atwix.com>
2014-10-31 12:50:22 +0200

    Bugfix for associated product edit page

--- app/code/community/Atwix/PVF/Model/Observer.php
+++ app/code/community/Atwix/PVF/Model/Observer.php
@@ -7,11 +7,22 @@ class Atwix_PVF_Model_Observer
         $_block = $observer->getBlock();
         $_type = $_block->getType();
         if ($_type == 'adminhtml/catalog_product_edit') {
-            $_deleteButton = $_block->getChild('delete_button');
+            
             $_block->setChild('product_view_button',
                 $_block->getLayout()->createBlock('atwix_pvf/adminhtml_widget_button')
             );
-            $_deleteButton->setBeforeHtml($_block->getChild('product_view_button')->toHtml());
+
+            $_deleteButton = $_block->getChild('delete_button');
+            /* Prepend the new button to the 'Delete' button if exists */
+            if (is_object($_deleteButton)) {
+                $_deleteButton->setBeforeHtml($_block->getChild('product_view_button')->toHtml());
+            } else {
+                /* Prepend the new button to the 'Reset' button if 'Delete' button does not exist */
+                $_resetButton = $_block->getChild('reset_button');
+                if (is_object($_resetButton)) {
+                    $_resetButton->setBeforeHtml($_block->getChild('product_view_button')->toHtml());
+                }
+            }
         }
     }
 }
